51d9035b31a5707a9543c35b83430320d9508022
@snookcoder <126566535+snookCoder@users.noreply.github.com>
2025-02-21 18:29:54 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
         port: "",
+        pathname: "/**", // Ensure this matches the path of your images
       },
     ],
   },
+  reactStrictMode: true, // Enable React Strict Mode in development
 };
 
 module.exports = nextConfig;
